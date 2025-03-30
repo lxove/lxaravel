1b1fd04fb92d8e36ceb176537581b3b3e8e987f5
Love <love.jonsson@ecsolutions.se>
2025-03-30 12:26:27 +0200

    initial commit

---
+++ src/Traits/EnumFromString.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace src\Traits;
+
+use ValueError;
+
+trait EnumFromString {
+    /**
+     * Returns an enum value if the string exactly matches an existing case for that Enum. Case sensitive.
+     *
+     * For non-backed Enums.
+     */
+    public static function from(string $str) {
+        try {
+            return collect(self::cases())
+                ->firstOrFail(fn (self $case) => $case->name == $str);
+        } catch (\Throwable $th) {
+            throw new ValueError("Invalid enum value: $str");
+        }
+    }
+}

0300ed784c360ae6caf2fe01af2e2f91136841a7
Love <love.jonsson@ecsolutions.se>
2025-03-31 15:17:26 +0200

    feat: add betterenum trait

---
+++ src/Traits/BetterEnum.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace lxove\lxaravel\Traits;
+
+trait BetterEnum {
+
+  use EnumFromString, EnumNames;
+
+  public function serialize(): string | null {
+    return $this->name;
+  }
+  
+  public function unserialize(string $str) {
+    return $this::from($str);
+  }
+
+  public function jsonSerialize(): mixed {
+    return $this->serialize();
+  }
+}
